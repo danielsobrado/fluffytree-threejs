16aa7f2c1094150dbad25b41fed8c8acb4611f31
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-08-01 18:32:35 +0800

    feat: add visible leaf detail to unified crowns

--- src/rendering/tree-mesh-builder.js
+++ src/rendering/tree-mesh-builder.js
@@ -1,14 +1,17 @@
 import * as THREE from 'three';
 import { BranchMeshBuilder } from './branch-mesh-builder.js';
 import { CrownVolumeBuilder } from './crown-volume-builder.js';
+import { LeafClusterBuilder } from './leaf-cluster-builder.js';
 
 export class TreeMeshBuilder {
   constructor({
     branchMeshBuilder = new BranchMeshBuilder(),
     crownVolumeBuilder = new CrownVolumeBuilder(),
+    leafClusterBuilder = new LeafClusterBuilder(),
   } = {}) {
     this.branchMeshBuilder = branchMeshBuilder;
     this.crownVolumeBuilder = crownVolumeBuilder;
+    this.leafClusterBuilder = leafClusterBuilder;
   }
 
   build(treeData) {
@@ -17,7 +20,12 @@ export class TreeMeshBuilder {
 
     const structure = this.branchMeshBuilder.build(treeData);
     const crown = this.crownVolumeBuilder.build(treeData);
+    const leafDetail = this.leafClusterBuilder.build(treeData);
     const volume = crown.geometry.userData.volume;
+    const leafMetrics = leafDetail.userData.leafDetail ?? {
+      clusterCount: 0,
+      leafCount: 0,
+    };
 
     group.userData.tree = {
       presetId: treeData.presetId,
@@ -26,8 +34,10 @@ export class TreeMeshBuilder {
       controlLobeCount: treeData.lobes.length,
       crownTriangleCount: volume.triangleCount,
       crownVertexCount: volume.vertexCount,
+      leafClusterCount: leafMetrics.clusterCount,
+      leafCount: leafMetrics.leafCount,
     };
-    group.add(structure, crown);
+    group.add(structure, crown, leafDetail);
     return group;
   }
 }
