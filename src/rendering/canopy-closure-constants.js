82fbcdec5737aeffaa5d2827182fdfd242317eb1
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-08-01 20:06:38 +0800

    feat: add canopy closure constants

---
+++ src/rendering/canopy-closure-constants.js
@@ -0,0 +1,11 @@
+export const CANOPY_CLOSURE_CONSTANTS = Object.freeze({
+  goldenAngle: Math.PI * (3 - Math.sqrt(5)),
+  minimumFieldInset: 0.025,
+  inwardCorrectionSteps: 6,
+  inwardCorrectionRatio: 0.34,
+  minimumScale: 0.035,
+  axisScaleMultiplier: 1.08,
+  bridgeScaleMultiplier: 0.94,
+  capScaleMultiplier: 0.88,
+  verticalWeightFloor: 0.08,
+});
