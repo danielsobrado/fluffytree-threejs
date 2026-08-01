c092f8dc0f6b89532e7c883e05f3ca69cf5f0853
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-08-01 19:12:02 +0800

    fix(foliage): make the crown proxy invisible in the color pass

--- src/rendering/crown-volume-material-factory.js
+++ src/rendering/crown-volume-material-factory.js
@@ -1,24 +1,16 @@
 import * as THREE from 'three';
-import { CROWN_VOLUME_RENDERING_CONSTANTS } from './crown-volume-rendering-constants.js';
 
 export class CrownVolumeMaterialFactory {
-  create(foliage) {
-    const emissive = new THREE.Color(foliage.palette[0]).multiplyScalar(
-      CROWN_VOLUME_RENDERING_CONSTANTS.emissiveScale,
-    );
-    const coreTint = new THREE.Color(1, 1, 1).multiplyScalar(
-      foliage.leafDetail.coreBrightness,
-    );
-    const material = new THREE.MeshStandardMaterial({
-      color: coreTint,
-      vertexColors: true,
-      roughness: CROWN_VOLUME_RENDERING_CONSTANTS.roughness,
-      metalness: 0,
-      emissive,
-      emissiveIntensity: CROWN_VOLUME_RENDERING_CONSTANTS.emissiveIntensity,
-      flatShading: false,
+  create() {
+    const material = new THREE.MeshBasicMaterial({
+      color: 0x000000,
+      colorWrite: false,
+      depthWrite: false,
+      depthTest: false,
+      fog: false,
+      toneMapped: false,
     });
-    material.name = 'inner-crown-core-material';
+    material.name = 'crown-shadow-proxy-material';
     return material;
   }
 }
