e6ac2c4ca86a0491df9b94619c33d057cb8f33d2
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-08-01 11:12:43 +0400

    refactor: centralize generation constants

---
+++ src/generation/generation-constants.js
@@ -0,0 +1,7 @@
+export const GENERATION_CONSTANTS = Object.freeze({
+  goldenAngle: Math.PI * (3 - Math.sqrt(5)),
+  minimumProfileRadius: 0.08,
+  minimumLobeScale: 0.18,
+  trunkSampleCount: 10,
+  branchSampleCount: 6,
+});
