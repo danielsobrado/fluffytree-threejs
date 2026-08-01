522fc7c6a33c60e1566daef8b5c36cabd7ebaae4
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-08-01 17:53:53 +0800

    refactor: centralize tree structure rendering constants

---
+++ src/rendering/tree-structure-rendering-constants.js
@@ -0,0 +1,7 @@
+export const TREE_STRUCTURE_RENDERING_CONSTANTS = Object.freeze({
+  radialSegments: 9,
+  trunkCurveSamples: 18,
+  branchCurveSamples: 10,
+  taperExponent: 0.78,
+  flareExponent: 4,
+});
