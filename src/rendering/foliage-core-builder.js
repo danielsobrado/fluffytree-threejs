4a8171c3d8ff78310cc3002e7b6933305008af4d
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-08-03 09:55:48 +0400

    feat: render core connectors in the existing instanced draw

--- src/rendering/foliage-core-builder.js
+++ src/rendering/foliage-core-builder.js
@@ -1,7 +1,10 @@
 import * as THREE from 'three';
 import { FoliageCoreGeometryFactory } from './foliage-core-geometry-factory.js';
+import { createFoliageCoreLayout } from './foliage-core-layout.js';
 import { FoliageCoreMaterialFactory } from './foliage-core-material-factory.js';
 import { addFoliageInstanceAttributes } from './instanced-foliage-attributes.js';
+
+const LOCAL_BRIDGE_AXIS = new THREE.Vector3(0, 1, 0);
 
 function crownDirection(position, center) {
   const x = position.x - center.x;
@@ -27,13 +30,24 @@ export class FoliageCoreBuilder {
 
   build(
     treeData,
-    { paletteTexture, sunDirection, detail = 1, scaleMultiplier = 1, name = 'foliage-core' },
+    {
+      paletteTexture,
+      sunDirection,
+      detail = 1,
+      lodIndex = 0,
+      scaleMultiplier = 1,
+      name = 'foliage-core',
+    },
   ) {
+    const layout = createFoliageCoreLayout(treeData, {
+      lodIndex,
+      scaleMultiplier,
+    });
     const geometry = this.geometryFactory.create(detail);
-    addFoliageInstanceAttributes(geometry, treeData.lobes, {
-      getExposure: (lobe) => treeData.lobeExposure[lobe.id] ?? 1,
-      getCrownDirection: (lobe) =>
-        crownDirection(lobe.position, treeData.crownCenter),
+    addFoliageInstanceAttributes(geometry, layout.instances, {
+      getExposure: (instance) => instance.exposure,
+      getCrownDirection: (instance) =>
+        crownDirection(instance.position, treeData.crownCenter),
     });
 
     const material = this.materialFactory.create({
@@ -44,24 +58,39 @@ export class FoliageCoreBuilder {
     const foliage = new THREE.InstancedMesh(
       geometry,
       material,
-      treeData.lobes.length,
+      layout.instances.length,
     );
     const matrix = new THREE.Matrix4();
     const position = new THREE.Vector3();
     const rotation = new THREE.Euler();
+    const direction = new THREE.Vector3();
     const quaternion = new THREE.Quaternion();
     const scale = new THREE.Vector3();
 
-    treeData.lobes.forEach((lobe, index) => {
-      position.set(lobe.position.x, lobe.position.y, lobe.position.z);
-      rotation.set(lobe.rotation.x, lobe.rotation.y, lobe.rotation.z);
-      quaternion.setFromEuler(rotation);
-      const coreScale = treeData.palette.core.scale * scaleMultiplier;
-      scale.set(
-        lobe.scale.x * coreScale,
-        lobe.scale.y * coreScale,
-        lobe.scale.z * coreScale,
+    layout.instances.forEach((instance, index) => {
+      position.set(
+        instance.position.x,
+        instance.position.y,
+        instance.position.z,
       );
+      if (instance.kind === 'bridge') {
+        direction
+          .set(
+            instance.direction.x,
+            instance.direction.y,
+            instance.direction.z,
+          )
+          .normalize();
+        quaternion.setFromUnitVectors(LOCAL_BRIDGE_AXIS, direction);
+      } else {
+        rotation.set(
+          instance.rotation.x,
+          instance.rotation.y,
+          instance.rotation.z,
+        );
+        quaternion.setFromEuler(rotation);
+      }
+      scale.set(instance.scale.x, instance.scale.y, instance.scale.z);
       matrix.compose(position, quaternion, scale);
       foliage.setMatrixAt(index, matrix);
     });
@@ -74,8 +103,13 @@ export class FoliageCoreBuilder {
     foliage.computeBoundingBox();
     foliage.computeBoundingSphere();
     foliage.userData.foliageCore = {
-      instanceCount: treeData.lobes.length,
+      instanceCount: layout.instances.length,
+      lobeInstanceCount: layout.lobeInstanceCount,
+      bridgeInstanceCount: layout.bridgeInstanceCount,
+      effectiveCoreScale: layout.effectiveCoreScale,
+      profile: layout.profile,
       detail,
+      lodIndex,
     };
     return foliage;
   }
