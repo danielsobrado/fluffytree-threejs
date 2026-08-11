efcb271c8fee024a158bcdd4f4befd662aa14952
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-08-11 14:26:18 +0400

    qa: validate solidity policy in browser

--- src/diagnostics/isolated-canopy-solidity-probe.js
+++ src/diagnostics/isolated-canopy-solidity-probe.js
@@ -1,10 +1,26 @@
+import { parseCanopySolidityQaConfig } from '../qa/canopy-solidity-qa-config.js';
 import {
   restoreObjectLodFade,
   snapshotObjectLodFade,
 } from '../rendering/lod-dither-fade.js';
 import { CanopySolidityProbe } from './canopy-solidity-probe.js';
 
+const SOLIDITY_CONFIG_SUFFIX = 'canopy-solidity-qa.yaml';
+
 export class IsolatedCanopySolidityProbe extends CanopySolidityProbe {
+  constructor(options = {}) {
+    super(options);
+    const loader = this.configLoader;
+    this.configLoader = {
+      load: async (url) => {
+        const config = await loader.load(url);
+        return String(url).endsWith(SOLIDITY_CONFIG_SUFFIX)
+          ? parseCanopySolidityQaConfig(config)
+          : config;
+      },
+    };
+  }
+
   beginIsolation(renderer, scene) {
     const previousRenderTarget = renderer.getRenderTarget();
     const restore = super.beginIsolation(renderer, scene);
