095e05c909ccdadf39f1104b8ebc4cf67773ae2e
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-08-18 03:15:01 +0400

    style: define palm bark banding constants

---
+++ src/rendering/tree-bark-style-constants.js
@@ -0,0 +1,14 @@
+export const TREE_BARK_PATTERNS = Object.freeze({
+  WOOD: 'wood',
+  PALM: 'palm',
+});
+
+export const PALM_BARK_STYLE = Object.freeze({
+  minimumBandCount: 7,
+  maximumBandCount: 12,
+  bandsPerMeter: 0.85,
+  ringStrength: 0.2,
+  verticalBreakupStrength: 0.07,
+  azimuthWarpStrength: 0.32,
+  baseDarkening: 0.16,
+});
