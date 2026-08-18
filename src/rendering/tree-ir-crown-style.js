2f3ca4d96bd90072a73ff358ccf6d51d7cc890dc
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-08-18 11:40:12 +0400

    style: shade crown cores by exposure

--- src/rendering/tree-ir-crown-style.js
+++ src/rendering/tree-ir-crown-style.js
@@ -3,14 +3,16 @@ import {
   treeIrStyleUnit,
 } from './tree-ir-style-random.js';
 
-export function calculateTreeIrCrownStyle(treeIr, volume, config) {
+export function calculateTreeIrCrownStyle(treeIr, volume, config, exposure = 1) {
   const variation = config.shapeVariation;
+  const depthBrightness = 1 - config.depthShading * (1 - exposure);
   return Object.freeze({
     scaleX: 1 + treeIrStyleSigned(treeIr, volume.id, 'crown-scale-x') * variation,
     scaleY: 1 + treeIrStyleSigned(treeIr, volume.id, 'crown-scale-y') * variation,
     scaleZ: 1 + treeIrStyleSigned(treeIr, volume.id, 'crown-scale-z') * variation,
     brightness:
       config.brightness *
+      depthBrightness *
       (0.96 + treeIrStyleUnit(treeIr, volume.id, 'crown-brightness') * 0.08),
   });
 }
